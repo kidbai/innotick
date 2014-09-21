057487aaf1bad83f75d97b28cf158ea110afb047
Youngbye <ybooty0@gmail.com>
2014-09-21 22:07:47 +0800

    update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,13 +15,62 @@ use yii\web\JqueryAsset;
 <?php $this->beginBody() ?>
     <div class="wrapper">
         <div id="top" class="column">
-            <div class="logo">innotick</div>
+            <div class="nav">
+                <div class="logo">
+                    <a href="#"><img src="/img/icon/logo.png" height="80" width="80" alt=""/></a>
+                </div>
+                <div class="menu">
+                   <ul>
+                       <li>产品</li>
+                       <li>观点</li>
+                       <li>专栏</li>
+                       <li>社区</li>
+                   </ul>
+                </div>
+                <div class="search">
+                    <div class="search-btn">
+                       <a href=""><img src="/img/icon/search.png" height="30" width="30" alt=""/> </a>
+                    </div>
+                    <div class="field">
+                        <input id="top-search-field" type="text" placeholder="搜索文章"/>
+                    </div>
+                </div>
+                <div class="login">
+                    <a href="javascript:;" class="btn">登陆</a>
+                </div>
+            </div>
+            <div class="navigation-cont">
+                <div class="navigation-main">
+                    <div class="navigation-left">
+                        
+                    </div>
+
+                </div>
+            </div>
         </div>
 
         <?= $content ?>
 
-        <div id="bottom" class="column">
-            <div class="logo">innotick</div>
+        <div id="bottom" class="column footer">
+            <div class="footer-cont">
+                <div class="footer-left">
+                   <div class="footer-left-up">
+                      左上 
+                   </div> 
+                   <div class="footer-left-down">
+                      左下 
+                   </div>
+                </div>
+                <div class="footer-mid">
+                   中 
+                </div>
+                <div class="footer-right">
+                   右 
+                </div>
+            </div>
+            <div class="footer-bottom">
+               <p class="footer-text">2013-2014 创新设计 浙ICP备13036478号-5</p> 
+            </div>
         </div>        
     </div>
 <?php $this->endBody() ?>
--- views/site/index.php
+++ views/site/index.php
@@ -1,11 +1,62 @@
-
-<div class="column">
-    <div class="col-5 left">
-
-    </div>
-    
-    <div class="col-10 right">
-
-    </div>
+<div id="content" class="wrapper">	
+	<div class="column content-1">
+		<div class="content-img">
+				
+		</div>	
+		<div class="content-nav">
+			<div class="content-btn">
+				<div class="content-btn-top">
+				</div>
+				<div class="content-btn-down">
+					<a href=""><p>最新</p></a>
+				</div>
+			</div>
+			<div class="content-smart">
+				<a href=""><p>智能</p></a>
+			</div>
+			<div class="content-list">
+				<ul class="content-list-ul">
+					<li><a href=""><p>小米</p></a></li>
+					<li><a href=""><p>Tesla</p></a></li>
+					<li><a href=""><p>Oculus</p></a></li>
+					<li><a href=""><p>Uber</p></a></li>
+					<li><a href=""><p>比特币</p></a></li>
+					<li><a href=""><p>更多</p></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="column content-2">
+	    <div class="col-11 content-col">
+	    	<div class="content-c">
+	    		
+	    	</div>
+	    	<div class="content-c">
+	    		
+	    	</div>
+	    	<div class="content-c">
+	    		
+	    	</div>
+	    	<div class="content-c">
+	    		
+	    	</div>
+	    	<div class="content-c">
+	    		
+	    	</div>
+	    	<div class="content-c">
+	    		
+	    	</div>
+	    </div>
+	    
+	    <div class="col-4">
+	    	<div class="focusNew">
+	    		<div class="new">
+	    			
+	    		</div>
+	    		<div class="new">
+	    			
+	    		</div>
+	    	</div>
+	    </div>
+	</div>
 </div>
-
