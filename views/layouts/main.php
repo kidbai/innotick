f43238ca8d6b2dd3c340cdd1cd3ddde6fd14dd60
Youngbye <ybooty0@gmail.com>
2014-09-22 19:12:53 +0800

    update the cont and footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,10 +21,10 @@ use yii\web\JqueryAsset;
                 </div>
                 <div class="menu">
                    <ul>
-                       <li>产品</li>
-                       <li>观点</li>
-                       <li>专栏</li>
-                       <li>社区</li>
+                       <a href="#"><li>产品</li></a>
+                       <a href="#"><li>观点</li></a>
+                       <a href="#"><li>专栏</li></a>
+                       <a href="#"><li>社区</li></a>
                    </ul>
                 </div>
                 <div class="search">
@@ -39,8 +39,8 @@ use yii\web\JqueryAsset;
                     <a href="javascript:;" class="btn">登陆</a>
                 </div>
             </div>
-            <div class="navigation-cont">
-                <div class="navigation-main">
+            <div class="carousel">
+                <div class="carousel-inner">
                     <div class="navigation-left">
                         
                     </div>
@@ -52,29 +52,56 @@ use yii\web\JqueryAsset;
         <?= $content ?>
 
         <div id="bottom" class="column footer">
-            <div class="footer-cont">
-                <div class="footer-left">
-                   <div class="footer-left-up">
-                      左上 
+            <div class="cont">
+                <div class="left">
+                   <div class="left-up">
+                        <div class="about">
+                            <label class="fs-18 orange">关于我们</label>
+                            <a class="fr fs-13 wt" href="#">了解更多>></a>
+                        </div>
+                        <p class="lightgray fl fs-14 lp-2">是一家全球视野的前沿科技媒体，提供关于中国于美国的最前沿科技创业资讯，致力于成为沟通者两个全球最大互联网/移动市场的互联网社区。</p>
                    </div> 
-                   <div class="footer-left-down">
-                      左下 
+                   <div class="left-down">
+                        <div class="about">
+                            <label class="fs-18 orange">联系我们</label>
+                            <a class="fr fs-13 wt" href="#">详细信息</a>
+                        </div>
+                        <p class="lightgray fl fs-14 lp-2">电话：400-633-5715</br>邮件：hi@chuangxinsheji.com</p>
                    </div>
                 </div>
-                <div class="footer-mid">
-                   中 
+                <div class="mid">
+                   <div class="about">
+                       <label class="fs-18 orange">加入我们</label>
+                       <a class="fr fs-13 wt" href="#">了解更多>></a>
+                   </div>
+                   <p class="lightgray fl fs-14 lp-2">我们欢迎那些聪明、热衷科技创新、视野开阔、反传统思维、能双语工作、行动力强、对创造高质量的科技新闻和评论充满兴趣的家伙加入团队，无论你在那里（不过北京与硅谷优先）。</p>
                 </div>
-                <div class="footer-right">
-                   右 
+                <div class="right">
+                        <div class="about">
+                            <label class="fs-18 orange">合作伙伴</label>
+                        </div>
+                        <p class="lightgray fl fs-14 lp-2">光华设计基金会</br>中国工业设计协会</br>创新设计工程实验室</br></p> 
                 </div>
-            </div>
-            <div class="footer-bottom">
-               <p class="footer-text">2013-2014 创新设计 浙ICP备13036478号-5</p> 
-            </div>
-        </div>        
+            
+        </div>    
+        <div class="bottom">
+            <p class="text fs-12 wt">&copy;2013-2014 创新设计 浙ICP备13036478号-5</p> 
+        </div>    
     </div>
+    <script>
+    $(function (){
+        $("#post").each(function(){
+            if($(this).mouseover()){
+                $(this).addClass("bg-click");
+            }
+            if($(this).mouseout()){
+                $(this).removeClass("bg-click");
+            }
+        });
+    });
+    </script>
 <?php $this->endBody() ?>
 </body>
-<script type="text/javascript" src="/js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
 </html>
 <?php $this->endPage() ?>
--- views/site/index.php
+++ views/site/index.php
@@ -1,21 +1,21 @@
 <div id="content" class="wrapper">	
-	<div class="column content-1">
-		<div class="content-img">
+	<div class="column content-up">
+		<div class="img">
 				
 		</div>	
-		<div class="content-nav">
-			<div class="content-btn">
-				<div class="content-btn-top">
+		<div class="nav">
+			<div class="btn">
+				<div class="btn-top">
 				</div>
-				<div class="content-btn-down">
+				<div class="btn-down">
 					<a href=""><p>最新</p></a>
 				</div>
 			</div>
-			<div class="content-smart">
+			<div class="smart">
 				<a href=""><p>智能</p></a>
 			</div>
-			<div class="content-list">
-				<ul class="content-list-ul">
+			<div class="list">
+				<ul class="list-ul">
 					<li><a href=""><p>小米</p></a></li>
 					<li><a href=""><p>Tesla</p></a></li>
 					<li><a href=""><p>Oculus</p></a></li>
@@ -26,37 +26,184 @@
 			</div>
 		</div>
 	</div>
-	<div class="column content-2">
-	    <div class="col-11 content-col">
-	    	<div class="content-c">
-	    		
-	    	</div>
-	    	<div class="content-c">
-	    		
-	    	</div>
-	    	<div class="content-c">
-	    		
-	    	</div>
-	    	<div class="content-c">
-	    		
-	    	</div>
-	    	<div class="content-c">
-	    		
-	    	</div>
-	    	<div class="content-c">
-	    		
-	    	</div>
-	    </div>
-	    
-	    <div class="col-4">
-	    	<div class="focusNew">
-	    		<div class="new">
-	    			
-	    		</div>
-	    		<div class="new">
-	    			
+	<div class="column content-down">
+	    <div class="col-11 left">
+	    	<div id="post" class="post">
+	    		<div class="img-holder">
+	    			<img src="" width="260" height="180"/>
+	    			<div class="icon-type">
+	    				<div style="background:url('/img/icon/tag3.png') no-repeat;width:39px;height:20px"><p class="fs-10">网站</p></div>
+	    			</div>
+	    		</div>	
+	    		<div class="text">
+	    			<p class="fs-21 lp-3">韩国电子优惠券服务Spoqa融资200万美元，并进入日本市场</p>	
+	    			<p class="fs-12 lp-1">作者<strong>Betty</strong> - 2014/08/26 <span class="orange">#移动互联网&nbsp;#电子商务&nbsp;#融资</span></p>
+	    			<p class="fs-14 lp-3">总部是在韩国首尔的创业公司Spoqa做的一种积分服务Dodo。<a class="" href="#"><strong>阅读全文</strong></a></p>
+	    			<div class="btn">
+	    				<a href="#"><img src="#" alt=""></a>
+	    				<a href="#"><img src="#" alt=""></a>
+	    				<a href="#"><img src="#" alt=""></a>
+	    			</div>	
 	    		</div>
 	    	</div>
+    				<HR align=left width=880 color=#d7d7d7 SIZE=2 noShade>
+	    	<div id="post" class="post">
+	    		<div class="img-holder">
+	    			<img src="" width="260" height="180"/>
+	    			<div class="icon-type">
+	    				<div style="background:url('/img/icon/tag2.png') no-repeat;width:39px;height:20px"><p class="fs-10">应用</p></div>
+	    			</div>
+	    		</div>	
+	    		<div class="text">
+	    			<p class="fs-21 lp-3">韩国电子优惠券服务Spoqa融资200万美元，并进入日本市场</p>	
+	    			<p class="fs-12 lp-1">作者<strong>Betty</strong> - 2014/08/26</p>
+	    			<p class="fs-14 lp-3">总部是在韩国首尔的创业公司Spoqa做的一种积分服务Dodo。<a class="" href="#"><strong>阅读全文</strong></a></p>
+	    			
+	    		</div>
+	    	</div>
+    				<HR align=left width=880 color=#d7d7d7 SIZE=2 noShade>
+	    	<div id="post" class="post">
+	    		<div class="img-holder">
+	    			<img src="" width="260" height="180"/>
+	    			<div class="icon-type">
+	    				<div style="background:url('/img/icon/tag4.png') no-repeat;width:39px;height:20px"><p class="fs-10">硬件</p></div>
+	    			</div>
+	    		</div>	
+	    		<div class="text">
+	    			<p class="fs-21 lp-3">韩国电子优惠券服务Spoqa融资200万美元，并进入日本市场</p>	
+	    			<p class="fs-12 lp-1">作者<strong>Betty</strong> - 2014/08/26</p>
+	    			<p class="fs-14 lp-3">总部是在韩国首尔的创业公司Spoqa做的一种积分服务Dodo。<a class="" href="#"><strong>阅读全文</strong></a></p>
+	    			
+	    		</div>
+	    	</div>
+    				<HR align=left width=880 color=#d7d7d7 SIZE=2 noShade>
+	    	<div id="post" class="post">
+	    		<div class="img-holder">
+	    			<img src="" width="260" height="180"/>
+	    			<div class="icon-type">
+	    				<div style="background:url('/img/icon/tag1.png') no-repeat;width:39px;height:20px"><p class="fs-10">网站</p></div>
+	    			</div>
+	    		</div>	
+	    		<div class="text">
+	    			<p class="fs-21 lp-3">韩国电子优惠券服务Spoqa融资200万美元，并进入日本市场</p>	
+	    			<p class="fs-12 lp-1">作者<strong>Betty</strong> - 2014/08/26</p>
+	    			<p class="fs-14 lp-3">总部是在韩国首尔的创业公司Spoqa做的一种积分服务Dodo。<a class="" href="#"><strong>阅读全文</strong></a></p>
+	    			
+	    		</div>
+	    	</div>
+    				<HR align=left width=880 color=#d7d7d7 SIZE=2 noShade>
+	    	<div id="post" class="post">
+	    		<div class="img-holder">
+	    			<img src="" width="260" height="180"/>
+	    			<div class="icon-type">
+	    				<div style="background:url('/img/icon/tag5.png') no-repeat;width:39px;height:20px"><p class="fs-10">网站</p></div>
+	    			</div>
+	    		</div>	
+	    		<div class="text">
+	    			<p class="fs-21 lp-3">韩国电子优惠券服务Spoqa融资200万美元，并进入日本市场</p>	
+	    			<p class="fs-12 lp-1">作者<strong>Betty</strong> - 2014/08/26</p>
+	    			<p class="fs-14 lp-3">总部是在韩国首尔的创业公司Spoqa做的一种积分服务Dodo。<a class="" href="#"><strong>阅读全文</strong></a></p>
+	    			
+	    		</div>
+	    	</div>
+    				<HR align=left width=880 color=#d7d7d7 SIZE=2 noShade>
+	    	<div id="post" class="post">
+	    		<div class="img-holder">
+	    			<img src="" width="260" height="180"/>
+	    			<div class="icon-type">
+	    				<div style="background:url('/img/icon/tag5.png') no-repeat;width:39px;height:20px"><p class="fs-10">网站</p></div>
+	    			</div>
+	    		</div>	
+	    		<div class="text">
+	    			<p class="fs-21 lp-3">韩国电子优惠券服务Spoqa融资200万美元，并进入日本市场</p>	
+	    			<p class="fs-12 lp-1">作者<strong>Betty</strong> - 2014/08/26</p>
+	    			<p class="fs-14 lp-3">总部是在韩国首尔的创业公司Spoqa做的一种积分服务Dodo。<a class="" href="#"><strong>阅读全文</strong></a></p>
+	    			
+	    		</div>
+	    	</div>
+    				<HR align=left width=880 color=#d7d7d7 SIZE=2 noShade>
+	    	
 	    </div>
+	   <!--中右内容--> 
+	    <div class="col-4 right">
+	    	<div class="hot-list">
+	    		<div class="hot">
+	    			<div class="color-line-up">
+	    				
+	    			</div>	
+	    			<div class="img-up">
+	    				<a href="#"><img src="#" alt=""></a>	
+	    			</div>
+	    			<p class="fs-14">PayPal联合创始人Peter Thie焦点哲学</p>
+	    		</div>
+	    		<div class="hot">
+	    			<div class="color-line-down">
+	    				
+	    			</div>	
+	    			<div>
+	    				<a href="#"><img src="#" alt=""></a>	
+	    			</div>
+	    			<p class="fs-14">PayPal联合创始人Peter Thie焦点哲学</p>
+	    			
+	    		</div>
+				<!-- 屏幕header-->
+	    		<div class="header">
+	    			<p class="fs-13">优质评论</p>
+	    			<HR align=left width=260 color=#ee6350 SIZE=2 noShade>
+	    		</div>
+
+	    		<div class="comment">
+	    			<div class="customer">
+	    				<p class="fs-14 orange fl">用户名</p>
+	    				<p class="fs-14 fl"> · 一小时前 </p>
+	    			</div>	
+	    			<div class="cont">
+	    				<p class="fs-14 lp-2">看了那么多有关于创业的例子，成功的，失败的都有，只想说：成功的路上没有太多可鉴和复制的。别人走过的路，你可以走，但走的方式和方法要更出色。没人走过的路，你也可以走，但走的很坎坷很纠结。</p>
+	    			</div>
+	    			<div class="from fs-15 lp-1">
+	    				<p class="fs-14 lp-1">评论于&nbsp;&nbsp;<a class="fs-14 lp-1" href="#">周鸿祎的互联网方法论</a></p>
+	    				<HR align=left width=260 color=#d7d7d7 SIZE=2 noShade>
+	    			</div>
+	    		</div>
+	    		<div class="comment">
+	    			<div class="customer">
+	    				<p class="fs-14 orange fl">用户名</p>
+	    				<p class="fs-14 fl"> · 一小时前 </p>
+	    			</div>	
+	    			<div class="cont">
+	    				<p class="fs-14 lp-2">看了那么多有关于创业的例子，成功的，失败的都有，只想说：成功的路上没有太多可鉴和复制的。别人走过的路，你可以走，但走的方式和方法要更出色。没人走过的路，你也可以走，但走的很坎坷很纠结。</p>
+	    			</div>
+	    			<div class="from fs-15 lp-1">
+	    				<p class="fs-14 lp-1">评论于&nbsp;&nbsp;<a class="fs-14 lp-1" href="#">周鸿祎的互联网方法论</a></p>
+	    				<HR align=left width=260 color=#d7d7d7 SIZE=2 noShade>
+	    			</div>
+	    		</div>
+	    		<div class="comment">
+	    			<div class="customer">
+	    				<p class="fs-14 orange fl">用户名</p>
+	    				<p class="fs-14 fl"> · 一小时前 </p>
+	    			</div>	
+	    			<div class="cont">
+	    				<p class="fs-14 lp-2">看了那么多有关于创业的例子，成功的，失败的都有，只想说：成功的路上没有太多可鉴和复制的。别人走过的路，你可以走，但走的方式和方法要更出色。没人走过的路，你也可以走，但走的很坎坷很纠结。</p>
+	    			</div>
+	    			<div class="from fs-15 lp-1">
+	    				<p class="fs-14 lp-1">评论于&nbsp;&nbsp;<a class="fs-14 lp-1" href="#">周鸿祎的互联网方法论</a></p>
+	    				<HR align=left width=260 color=#d7d7d7 SIZE=2 noShade>
+	    			</div>
+	    		</div>
+				
+				<!-- 二维码-->
+				<div class="qrcode">
+					<p class="text fs-12 lp-2">微信公众平台：搜索“创新设计”或扫描一下二维码:</p>
+					<img src="#" alt="">
+				</div>
+
+	    	</div>
+	    </div>
+	</div>
+	<div class="clear-0"></div>
+	<div class="load loadgray">
+		<p class="fs-14 fl loadnext">正在为您加载第X页面</p>	
+		<a class="fs-14 fr" href="#">下一页</a>
 	</div>
 </div>
