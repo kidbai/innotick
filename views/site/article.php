f703020c2bd5e2cba4dcb0239f8736dffd8cca1b
Youngbye <ybooty0@gmail.com>
2014-09-23 20:24:54 +0800

    add icons, article.php

---
+++ views/site/article.php
@@ -0,0 +1,167 @@
+<div id="content" class="wrapper">
+	<div class="column content-down bg-click">
+		<div class="offset">
+			<div class="left col-11">
+				<img src="/img/icon/article.png" width="800" height="179" alt=""/>
+				<div class="main bg-wt">
+					<div class="text mt-20">
+						<p class="fs-16">如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端如果你打开领英的个人页面，你回发现其顶端</p>
+					</div>
+					<div class="icon divider">
+						<ul class="ml-40 mt-70">
+							<li class="fl ml-20"><a href="#"><img src="/img/icon/like.png" width="25px" height="25px" alt=""/><span class="fs-15 ml-5">(45)</span></a></li>
+							<li class="fl ml-20"><a href="#"><img src="/img/icon/dislike.png" width="25px" height="25px" alt=""/><span class="fs-15 ml-5">(45)</span></a></li>
+							<li class="fl ml-20"><a href="#"><img src="/img/icon/share.png" width="25px" height="23px" alt=""/><span class="fs-15 ml-5">(45)</span></a></li>
+							<li class="fl ml-20"><a href="#"><img src="/img/icon/collected.png" width="25px" height="23px" alt=""/><span class="fs-15 ml-5">(45)</span></a></li>
+							<li class="sbtn orange ml-130 fl"><a href="#"><p class="orange fs-15">分享到微信</p></a></li>
+							<li class="sbtn grass ml-20 fl"><a href="#"><p class="grass fs-15">分享到微博</p></a></li>
+						</ul>
+					</div>
+					<div class="link divider">
+						<img src="/img/icon/keyword.png" width="17" height="17" alt="" class="fl ml-60 mt-20"/>
+						<p class="fl fs-15">&nbsp;&nbsp;#&nbsp;<a class="orange fs-15" href="#">LinkedIn</a>&nbsp;#&nbsp;<a class="orange fs-15" href="#">领英</a></p>
+					</div>
+					<div class="designer divider">
+						<div class="circle ml-60 mt-20 fl"></div>
+						<div class="description fl mt-30 ml-10">
+							<p class="fs-14 fl l-designer">作者用户名</p>
+							<div class="wrap-designer fl bg-designer ml-10">
+								<p class="fl ml-10 wt">设计师</p>
+							</div>
+							<div class="clear"></div>
+							<p class="l-designer">人生路，真慌张。WeChat: zhangyuxin87</p>
+						</div>
+					</div>
+					<div class="comment-board divider">
+						<p class="l-designer fs-14 ml-60 mt-20">文章评论(10)</p>
+						<textarea class="fs-14 mt-15 ml-60 " cols="95" rows="5" placeholder="你怎么看"></textarea>
+						<p class="fl l-designer ml-60 mt-15"><a class="fs-14 l-designer" href="#">登陆</a>&nbsp;&nbsp;后参与讨论</p>
+						<a class="btn orange mt-8 ml-487" href=""><p class="fs-14 wt">提交评论</p></a>
+					</div>
+					<div class="comment">
+						<div class="block divider">
+							<div class="circle mt-20 fl"></div>
+							<div class="fl mt-20 ml-10 text">
+								<div class="fl"><p class="fs-14 orange">人生路</p></div>
+								<div class="fl ml-10"><p class="l-designer fs-13">· 3小时前</p></div>
+								<div class="fr"><a href="#"><img class="fl" src="/img/icon/floor.png" width="15" height="15" alt=""/><p class="l-designer fs-13 fl">1楼</p></a></div>
+								<div class="clear"></div>
+								<p class="l-designer fs-14 mt-12 fl">设计不错哦</p>
+								<ul class="fr mt-13">
+									<li class="fl"><a href="#"><p class="fs-13 l-designer">顶(1)</p></a></li>
+									<li class="fl ml-15"><a href="#"><p class="fs-13 l-designer">踩(2)</p></a></li>
+								</ul>
+							</div>
+						</div>	
+						<div class="block divider">
+							<div class="circle mt-20 fl"></div>
+							<div class="fl mt-20 ml-10 text">
+								<div class="fl"><p class="fs-14 orange">人生路</p></div>
+								<div class="fl ml-10"><p class="l-designer fs-13">· 3小时前</p></div>
+								<div class="fr"><a href="#"><img class="fl" src="/img/icon/floor.png" width="15" height="15" alt=""/><p class="l-designer fs-13 fl">1楼</p></a></div>
+								<div class="clear"></div>
+								<p class="l-designer fs-14 mt-12 fl">设计不错哦</p>
+								<ul class="fr mt-13">
+									<li class="fl"><a href="#"><p class="fs-13 l-designer">顶(1)</p></a></li>
+									<li class="fl ml-15"><a href="#"><p class="fs-13 l-designer">踩(2)</p></a></li>
+								</ul>
+							</div>	
+						</div>	
+						<div class="block divider">
+							<div class="circle mt-20 fl"></div>
+							<div class="fl mt-20 ml-10 text">
+								<div class="fl"><p class="fs-14 orange">人生路</p></div>
+								<div class="fl ml-10"><p class="l-designer fs-13">· 3小时前</p></div>
+								<div class="fr"><a href="#"><img class="fl" src="/img/icon/floor.png" width="15" height="15" alt=""/><p class="l-designer fs-13 fl">1楼</p></a></div>
+								<div class="clear"></div>
+								<p class="l-designer fs-14 mt-12 fl">设计不错哦</p>
+								<ul class="fr mt-13">
+									<li class="fl"><a href="#"><p class="fs-13 l-designer">顶(1)</p></a></li>
+									<li class="fl ml-15"><a href="#"><p class="fs-13 l-designer">踩(2)</p></a></li>
+								</ul>
+							</div>	
+						</div>	
+					</div>
+				</div>
+			</div>	
+			<div class="right col-4">
+				<div class="hot-list bg-wt">
+		    		<div class="hot">
+		    			<div class="img-line-up">
+		    				
+		    			</div>	
+		    			
+		    				<a href="#"><img src="#" alt=""/></a>	
+		    			
+		    			<p class="fs-14">PayPal联合创始人Peter Thie焦点哲学</p>
+		    		</div>
+		    		<div class="hot">
+		    			<div class="img-line-down">
+		    				
+		    			</div>	
+		    			<div>
+		    				<a href="#"><img src="#" alt=""/></a>	
+		    			</div>
+		    			<p class="fs-14">PayPal联合创始人Peter Thie焦点哲学</p>
+		    			
+		    		</div>
+					<!-- 屏幕header-->
+		    		<div class="header">
+		    			<p class="fs-13">优质评论</p>
+		    		</div>
+		    		<HR align=center width=86.66666667% color=#ee6350 SIZE=2 style="margin-left:20px;" noShade>
+					<div class="small-article ml-20">
+						<div class="bg-click fl mt-10">
+							<img src="#" alt=""/>
+						</div>
+						<div class="text fl">
+							<p class="fs-15 lp-2 mt-5 ml-5">周鸿祎的互联网方法论</br>互网法论</p>
+						</div>
+					</div>
+					<div class="small-article ml-20">
+						<div class="bg-click fl mt-10">
+							<img src="#" alt=""/>
+						</div>
+						<div class="text fl">
+							<p class="fs-15 lp-2 mt-5 ml-5">周鸿祎的互联网方法论</br>互网法论</p>
+						</div>
+					</div>
+					<div class="small-article ml-20">
+						<div class="bg-click fl mt-10">
+							<img src="#" alt=""/>
+						</div>
+						<div class="text fl">
+							<p class="fs-15 lp-2 mt-5 ml-5">周鸿祎的互联网方法论</br>互网法论</p>
+						</div>
+					</div>
+					<div class="small-article ml-20">
+						<div class="bg-click fl mt-10">
+							<img src="#" alt=""/>
+						</div>
+						<div class="text fl">
+							<p class="fs-15 lp-2 mt-5 ml-5">周鸿祎的互联网方法论</br>互网法论</p>
+						</div>
+					</div>
+					<div class="small-article ml-20">
+						<div class="bg-click fl mt-10">
+							<img src="#" alt=""/>
+						</div>
+						<div class="text fl">
+							<p class="fs-15 lp-2 mt-5 ml-5">周鸿祎的互联网方法论</br>互网法论</p>
+						</div>
+					</div>
+		    		
+		    		
+		    		
+					
+					<!-- 二维码-->
+					<div class="qrcode">
+						<p class="text fs-12 lp-2">微信公众平台：搜索“创新设计”或扫描一下二维码:</p>
+						<img src="#" alt=""/>
+					</div>
+
+		    	</div>
+			</div>
+		</div>
+	</div>	
+</div>
